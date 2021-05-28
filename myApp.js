4d825cc15b5e944b2eb233bdc613bc2f852697d3
Miu <miyuuu0728@gmail.com>
2021-05-28 09:28:12 -0700

    q6

--- myApp.js
+++ myApp.js
@@ -60,7 +60,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, id) => {
+    if (err) return console.log(err);
+    done(null, id);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
