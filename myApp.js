929cb0711979189ab23081c37824072af8115bbb
Miu <miyuuu0728@gmail.com>
2021-05-27 21:21:15 -0700

    added schema

--- myApp.js
+++ myApp.js
@@ -6,6 +6,14 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
 let Person;
 
 const createAndSavePerson = (done) => {
